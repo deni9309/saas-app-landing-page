53a31d8f188b94d32be91b9c5cf49e8c6156695b
Denitsa Slavkova <deni9309@gmail.com>
2024-09-29 22:20:18 +0300

    Initial Commit

---
+++ constants/index.js
@@ -0,0 +1,26 @@
+import {
+  features,
+  details,
+  faq,
+  plans,
+  testimonials,
+  logos,
+  links,
+  socials,
+} from './basic-constants'
+import { Web, Android, Ios, Windows } from './jsx-constants'
+
+export {
+  features,
+  details,
+  faq,
+  plans,
+  testimonials,
+  logos,
+  links,
+  socials,
+  Web,
+  Android,
+  Ios,
+  Windows,
+}
