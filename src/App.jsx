e4e1905dd0866aabe07a6fdac12b58ebb5b27b03
Denitsa Slavkova <deni9309@gmail.com>
2024-10-01 00:09:42 +0300

    Create hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from './sections/header'
+import Hero from './sections/hero'
 
 const App = () => {
   return (
     <main className="overflow-hidden ">
       <Header />
+      <Hero />
     </main>
   )
 }
---
+++ src/components/custom-button.jsx
@@ -0,0 +1,58 @@
+import { cn } from '../../lib/utils'
+import Marker from './marker'
+
+const CustomButton = ({
+  icon,
+  iconTitle,
+  markerFill,
+  children,
+  href,
+  containerClassNames,
+  onClick,
+}) => {
+  const Inner = () => (
+    <>
+      <span className="relative flex items-center min-h-[60px] px-4 g4 rounded-2xl inner-before group-hover:before:opacity-100 overflow-hidden">
+        <span className="absolute -left-[1px]">
+          <Marker />
+        </span>
+        {icon && (
+          <img
+            src={icon}
+            alt={iconTitle ?? 'circle'}
+            className="size-10 mr-5 object-contain z-10"
+          />
+        )}
+        <span className="relatize z-2 font-poppins base-bold text-p1 uppercase">
+          {children}
+        </span>
+      </span>
+
+      <span className="glow-before glow-after" />
+    </>
+  )
+
+  return href ? (
+    <a
+      href={href}
+      className={cn(
+        'relative p-0.5 g5 rounded-2xl shadow-500 group',
+        containerClassNames,
+      )}
+    >
+      <Inner />
+    </a>
+  ) : (
+    <button
+      onClick={onClick}
+      className={cn(
+        'relative p-0.5 g5 rounded-2xl shadow-500 group',
+        containerClassNames,
+      )}
+    >
+      <Inner />
+    </button>
+  )
+}
+
+export default CustomButton
